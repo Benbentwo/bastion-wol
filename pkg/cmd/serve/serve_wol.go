9ccb82ac7094c628a4b57eb70a9ab3ab04c330a5
Benjamin Smith <ben.smith.developer@gmail.com>
2020-10-02 09:48:23 -0700

    wake on lan works - loads either mac query param or env var

--- pkg/cmd/serve/serve_wol.go
+++ pkg/cmd/serve/serve_wol.go
@@ -1,49 +1,45 @@
 package serve
-//
-//import (
-//	"flag"
-//	"fmt"
-//	"github.com/Benbentwo/utils/log"
-//	"github.com/mdlayher/wol"
-//	"net"
-//	"net/http"
-//)
-//
-//func WolPage(w http.ResponseWriter, r *http.Request) {
-//	fmt.Fprintf(w, "{ \"status\": \"ok\" }")
-//	log.Logger().Infof("%s: WOL Page Pinged by: %s", now(), r.Host)
-//}
-//
-//// Run the wake command.
-//func wakeCmd(targetFlag string) error {
-//	flag.Parse()
-//
-//	target, err := net.ParseMAC(targetFlag)
-//	if err != nil {
-//		log.Logger().Fatal(err)
-//	}
-//
-//	// Can only do raw or UDP mode, not both.
-//	if *addrFlag != "" && *ifaceFlag != "" {
-//		log.Fatalf("must set '-a' or '-i' flag exclusively")
-//	}
-//
-//	if *ifaceFlag != "" {
-//		if err := wakeRaw(*ifaceFlag, target, password); err != nil {
-//			log.Fatal(err)
-//		}
-//
-//		log.Printf("sent raw Wake-on-LAN magic packet using %s to %s", *ifaceFlag, *targetFlag)
-//		return
-//	}
-//
-//	if err := wakeUDP(*addrFlag, target, password); err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	log.Printf("sent UDP Wake-on-LAN magic packet using %s to %s", *addrFlag, *targetFlag)
-//}
-//
+
+import (
+	"fmt"
+	"github.com/Benbentwo/utils/log"
+	"github.com/mdlayher/wol"
+	"net"
+	"net/http"
+	"os"
+)
+
+func WolPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "{ \"status\": \"ok\" }")
+	log.Logger().Infof("%s: WOL Page Pinged by: %s", now(), r.RemoteAddr)
+	macAddr := r.URL.Query().Get("mac")
+	if macAddr == "" {
+		val, exists := os.LookupEnv("MAC_ADDR_TO_WAKE")
+		if exists && val != "" {
+			macAddr = val
+		} else {
+			log.Logger().Errorf("Misconfigured server - no default mac address to send to")
+		}
+	}
+
+	_ = wakeCmd(macAddr)
+}
+
+// Run the wake command.
+func wakeCmd(macAddress string) error {
+	mac, err := net.ParseMAC(macAddress)
+	if err != nil {
+		log.Logger().Errorf("convert mac address %s: %s", macAddress, err)
+	}
+	password := make([]byte, 0)
+	if err := wakeUDP("255.255.255.255:9", mac, password); err != nil {
+		log.Logger().Fatal(err)
+	}
+
+	log.Logger().Printf("sent UDP Wake-on-LAN magic packet using %s to %s", macAddress, "local network")
+	return nil
+}
+
 //func wakeRaw(iface string, target net.HardwareAddr, password []byte) error {
 //	ifi, err := net.InterfaceByName(iface)
 //	if err != nil {
@@ -59,14 +55,14 @@ package serve
 //	// Attempt to wake target machine.
 //	return c.WakePassword(target, password)
 //}
-//
-//func wakeUDP(addr string, target net.HardwareAddr, password []byte) error {
-//	c, err := wol.NewClient()
-//	if err != nil {
-//		return err
-//	}
-//	defer c.Close()
-//
-//	// Attempt to wake target machine.
-//	return c.WakePassword(addr, target, password)
-//}
+
+func wakeUDP(addr string, target net.HardwareAddr, password []byte) error {
+	c, err := wol.NewClient()
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+
+	// Attempt to wake target machine.
+	return c.WakePassword(addr, target, password)
+}
